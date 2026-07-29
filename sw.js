80e3a57fccc35380451361f6d8d9151d1350e44b
Nor <70202921+philosopherwaterhead@users.noreply.github.com>
2026-07-29 21:21:16 +0900

    Refactor service worker for installation and activation

--- sw.js
+++ sw.js
@@ -8,15 +8,9 @@ const urlsToCache = [
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+self.addEventListener("activate", event => {
+  event.waitUntil(clients.claim());
 });
